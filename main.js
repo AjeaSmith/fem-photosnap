95101649d21eb6a9d3a24cbdcb0a2f7f6bd4b633
ajea <smithajea@rocketmail.com>
2024-04-22 10:59:08 -0400

    pricing: finished yearly and pricing columns

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Navigation logic
 const primaryHeader = document.querySelector(".header");
 const navToggle = document.querySelector(".header__toggle");
 const primaryNav = document.querySelector(".navigation");
@@ -19,4 +20,27 @@ navToggle.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
 	hamburgerIcon.style.display = "block";
 	closeIcon.style.display = "none";
+});
+
+// Pricing switch logic
+const toggleSwitch = document.getElementById("toggle");
+const monthlyPlans = document.querySelector(".monthly-plans");
+const yearlyPlans = document.querySelector(".yearly-plans");
+const monthlyHeading = document.querySelector(".monthly-heading");
+const yearlyHeading = document.querySelector(".yearly-heading");
+
+toggleSwitch.addEventListener("change", function () {
+	if (this.checked) {
+		monthlyHeading.style.opacity = "0.3";
+		yearlyHeading.style.opacity = "1";
+		// Switch to yearly plans
+		monthlyPlans.classList.add("hidden");
+		yearlyPlans.classList.remove("hidden");
+	} else {
+		monthlyHeading.style.opacity = "1";
+		yearlyHeading.style.opacity = "0.3";
+		// Switch to monthly plans
+		monthlyPlans.classList.remove("hidden");
+		yearlyPlans.classList.add("hidden");
+	}
 });
